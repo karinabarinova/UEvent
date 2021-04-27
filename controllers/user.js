8e6fe13d1b95a6f92f05063cb9dcdc0c940b8382
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-04-27 17:50:42 +0300

    integrated stripe payments, installed stripe, added list of event user has been subscribed to

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const service = require('../services/user');
 const authJwt = require('../middleware/authJwt');
+const stripePublicKey = process.env.STRIPE_PUBLISHABLE_KEY;
 
 // router.post('/add', authJwt.verifyToken, add);
 router.get('/',  authJwt.verifyToken, getUserInfo);
+router.get('/cart', authJwt.verifyToken, getCart);
+router.post('/purchase', authJwt.verifyToken, purchase);
+
 // router.get('/:id', getById);
 // router.patch('/:id', authJwt.verifyToken, update);
 // router.delete('/:id', authJwt.verifyToken, _delete);
@@ -15,5 +19,20 @@ module.exports = router;
 function getUserInfo(req, res, next) {
     service.getUserInfo(req.userId)
         .then((data) => res.json(data))
+        .catch(next);
+}
+
+function getCart(req, res, next) {
+    res.json({
+        stripePublicKey
+    })
+}
+
+function purchase(req, res, next) {
+    //we should get an array of objects to be purchased 
+    //{id, price, quantity}
+    const {items, stripeTokenId} = req.body;
+    service.purchase(req.userId, items, stripeTokenId)
+        .then(() => res.json({message: 'Successfully purchased event(s)'}))
         .catch(next);
 }
--- services/user.js
+++ services/user.js
@@ -1,7 +1,11 @@
-const {User} = require('../sequelize/models');
+const {User, Event} = require('../sequelize/models');
+const sendEmail = require('../helpers/sendMail');
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripe = require('stripe')(stripeSecretKey);
 
 module.exports = {
-    getUserInfo
+    getUserInfo,
+    purchase
 }
 
 async function getUserInfo(userId) {
@@ -10,6 +14,61 @@ async function getUserInfo(userId) {
     return {
         user,
         companies: await user.getCompanies(),
+        events: await user.getEvents()
         // events: await user.getEvents()
     };
 }
+
+async function purchase(userId, items, token) {
+    let amount = 0;
+    const test_token = "tok_visa";
+    items.forEach(item => {
+        amount = amount + item.price * item.quantity
+    });
+    
+    const paymentDetails = await stripe.charges.create({
+        amount: amount * 100,
+        source: test_token, //should be token
+        currency: 'usd' //should be changed dynamically or we will accept only USD
+    })
+    if (paymentDetails.status !== 'succeeded') {
+        console.log("Payment details", paymentDetails);
+        throw 'Failed payment';
+    }
+    const user = await User.findByPk(userId);
+    items.forEach(async item => {
+        const event = await Event.findByPk(item.id);
+        await event.addUser(user);
+        await user.addEvent(event); //maybe this step is not required
+        await Subscription.create({
+            eventId: item.id,
+            userId: user.id,
+            orderId: paymentDetails.metadata.order_id,
+            email: user.email
+            //send_notifications
+        })
+    })
+    //add events to user
+    //add user to events
+    //add subscription to user
+    //send email
+    await sendSubscriptionEmail(user);
+}
+
+//helpers
+
+async function sendSubscriptionEmail(user) {
+    let message = `<p>Hey ${capitalizeFirstLetter(user.firstName)}, you've subscribed to a new event(s)</p>
+                   <p></p>`;
+
+    await sendEmail({
+        to: user.email,
+        subject: 'Successful Payment for =event=',
+        html: `<h4>Successful Payment for =event=</h4>
+               ${message}`
+    });
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
