ccf5dda6b93a698a904b8ba4306752ee5925e0ad
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-07-22 15:36:15 +0300

    added fullcalendar with events

--- front/src/App.js
+++ front/src/App.js
@@ -20,6 +20,7 @@ import ResetPage from './pages/reset';
 import Auth from './store/auth/auth';
 import { CartStateProvider } from "./lib/cartState";
 import Order from "./components/Order";
+import Calendar from "./components/Calendar";
 import FeedbackWidget from "./components/Feedback";
 import CookieConsentWidget from "./components/CookieConsentWidget";
 
@@ -29,6 +30,7 @@ class App extends Component {
     let routes = (
     	<Switch>
     		<Route path='/signin' exact component={SingIn}/>
+        <Route path='/calendar' exact component={Calendar}/>
     		<Route path='/reset' exact component={ResetPage} />
     		<Route path='/new-event' exact component={CreateProduct}/>
     		<Route path='/new-company' exact component={CreateCompany}/>
